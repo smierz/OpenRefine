8dbdd528da98bdd27fc3f348ad3059e8bcd319ee
smierz <sandrasmails@gmail.com>
2025-09-30 15:27:23 +0200

    create dbConfig from container params

--- extensions/database/tests/src/com/google/refine/extension/database/mysql/MySQLDatabaseIT.java
+++ extensions/database/tests/src/com/google/refine/extension/database/mysql/MySQLDatabaseIT.java
@@ -4,22 +4,39 @@ package com.google.refine.extension.database.mysql;
 import org.testcontainers.containers.MySQLContainer;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+
+import com.google.refine.extension.database.DatabaseConfiguration;
 
 public class MySQLDatabaseIT {
 
+    // container setup
     private static final String dockerImage = "mysql:8.0";
 
+    private static final int DB_PORT = 3306;
+
     private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>(dockerImage)
-            .withExposedPorts(3306)
+            .withExposedPorts(DB_PORT)
             .withDatabaseName("test_db")
             .withUsername("mysql_root")
             .withPassword("secret")
             .withInitScript("mysql/init.sql");
 
+    // application setup
+    private static DatabaseConfiguration testDbConfig;
+
     @BeforeClass
     public static void startContainer() {
         mySQLContainer.start();
+
+        // set application parameters for db connection from container setup
+        testDbConfig = new DatabaseConfiguration();
+        testDbConfig.setDatabaseHost(mySQLContainer.getHost());
+        testDbConfig.setDatabasePort(mySQLContainer.getMappedPort(DB_PORT));
+        testDbConfig.setDatabaseType(MySQLDatabaseService.DB_NAME);
+        testDbConfig.setDatabaseName(mySQLContainer.getDatabaseName());
+        testDbConfig.setDatabasePassword(mySQLContainer.getPassword());
+        testDbConfig.setDatabaseUser(mySQLContainer.getUsername());
+        testDbConfig.setUseSSL(false);
     }
 
     @AfterClass
@@ -27,12 +44,7 @@ public class MySQLDatabaseIT {
         mySQLContainer.stop();
     }
 
-    @Test
-    public void test() {
-        System.out.println(mySQLContainer.getDatabaseName());
-        System.out.println(mySQLContainer.getUsername());
-        System.out.println(mySQLContainer.getPassword());
-        System.out.println(mySQLContainer.getJdbcUrl());
-        System.out.println(mySQLContainer.getTestQueryString());
+    static DatabaseConfiguration getTestDbConfig() {
+        return testDbConfig;
     }
 }
